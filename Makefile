834028ae9d072a16c78abba0d29203ae57bac424
Jesus Quiaro <jesusswordpvp10@gmail.com>
2024-03-24 13:41:11 +0100

    mod_sunday_24/03

--- Makefile
+++ Makefile
@@ -13,11 +13,11 @@ SRCS = ft_isalpha.c \
 	ft_substr.c \
 	ft_memmove.c \
 	ft_strlcpy.c \
+	ft_strlcat.c \
 	ft_toupper.c \
 	ft_tolower.c \
 	ft_strncmp.c \
 	ft_memcmp.c \
-	ft_strlcat.c \
 	ft_strchr.c \
 	ft_atoi.c \
 	ft_strrchr.c \
---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquiaro- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 14:06:50 by jquiaro-          #+#    #+#             */
+/*   Updated: 2024/03/24 12:37:27 by jquiaro-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (src_len + dstsize);
+	else
+	{
+		i = 0;
+		while (src[i] && dst_len + i < dstsize - 1)
+		{
+			dst[dst_len + i] = src[i];
+			i++;
+		}
+		dst[dst_len + i] = '\0';
+		return (dst_len + src_len);
+	}
+}
