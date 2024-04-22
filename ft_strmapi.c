7a03a452dedb7d4db3e8f098d4b2ed5c7ade8fa7
Jesús Alejandro Quiaro <jesusswordpvp10@gmail.com>
2024-04-22 16:11:43 +0200

    strmapi_and_striteri_22_04

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,28 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jquiaro- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 03:34:57 by jquiaro-          #+#    #+#             */
-/*   Updated: 2024/04/19 04:27:47 by jquiaro-         ###   ########.fr       */
+/*   Created: 2024/04/22 15:14:40 by marvin            #+#    #+#             */
+/*   Updated: 2024/04/22 15:14:40 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char			*str;
 	unsigned int	i;
 
 	i = 0;
-	if (s && f)
+	if (!s && !f)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	while (s[i])
 	{
-		while (s[i])
-		{
-			f(i, &s[i]);
-			i++;
-		}
+		str[i] = f(i, s[i]);
+		i++;
 	}
+	str[i] = '\0';
+	return (str);
 }
